ea9a0694cb6c24ef625552c5ad8c40e8c247e9a1
kelly.figueira <kelly.figueira@DBS-NOTE-565>
2020-12-14 22:47:28 -0300

    projeto bytebank

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(384, 348934);
+            Console.WriteLine(conta.Numero);
+
+            Console.ReadLine();
+        }
+    }
+}
